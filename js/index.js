c07cd97e4ba825813677d4e4355cac0d1201354d
Ashiqur Rahman <ashikmollah51@gmail.com>
2021-06-19 09:57:59 +0600

    Toggle navbar added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
+window.onscroll = function () { scrollFunction() };
 
+function scrollFunction() {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        document.getElementById("header").style.padding = "10px 0px";
+
+    } else {
+        document.getElementById("header").style.padding = "25px 0px";
+    }
+}
+
+function tabFunction () {
+    document.querySelector(".tabtext p").innerHTML = "What is up good people";
+}
+
+
+
+
+
+
+// Menu Toggle
+
+var nav = document.getElementById("nav");
+var navUl = document.getElementById("ul");
+
+nav.style.maxHeight = "100px";
+// navUl.style.maxHeight = "none";
+
+function toggleMenu () {
+    if (nav.style.maxHeight == "100px") {
+        nav.style.maxHeight = "300px";
+        navUl.style.display = "block";
+
+    } else {        
+    nav.style.maxHeight = "100px";
+    navUl.style.display = "none";
+    }
+
+}
